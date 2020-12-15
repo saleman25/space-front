7bca63febf2e190fda0e75373038eebf4f516358
Samantha Aleman <samantha.aleman25@gmail.com>
2020-12-14 23:01:43 -0800

    fixed errors so that things can be saved properly and rendered the way i want

--- src/actions/technologyActions.js
+++ src/actions/technologyActions.js
@@ -4,17 +4,18 @@
 
 export const getTechnology = () => {
     return (dispatch) => {
-       return fetch(`https://api.nasa.gov/techport/api/projects?active&api_key=bS7Kb4VfcpKaKmarzxrfcYlg8CJGzJ9m6CIajasm`)
+       return fetch(`https://api.nasa.gov/techport/api/projects/33080?api_key=bS7Kb4VfcpKaKmarzxrfcYlg8CJGzJ9m6CIajasm`)
         .then((resp) => resp.json())
         .then((result) =>{
+            console.log(result)
         const techObj = {
-            title: result.title,
-            description: result.description,
-            startDate: result.start_date,
-            endDate: result.end_date,
-            status: result.status
+            title: result.project.title,
+            description: result.project.description,
+            startDate: result.project.startDate,
+            endDate: result.project.endDate,
+            status: result.project.status
         }
-        dispatch({ type: "FETCH_TECHNOLOGY", payload: techObj.slice(0,5)})
+        dispatch({ type: "FETCH_TECHNOLOGY", payload: techObj })
         }
         );
     };
--- src/components/Technology.js
+++ src/components/Technology.js
@@ -7,6 +7,7 @@ class Technology extends Component {
 // fetch call 
 componentDidMount(){
     this.props.getTechnology();
+    console.log(this.props)
 }
 
 // another function abt the loading of the tech which is whats gonna render
@@ -16,7 +17,8 @@ render(){
     return (
         <div>
             <h1>{this.props.technology.title}</h1>
-            <h2>{this.props.technology.status}</h2>
+    <p>{this.props.technology.startDate} - {this.props.technology.endDate}</p>
+            <h3>{this.props.technology.status}</h3>
             <p>{this.props.technology.description}</p>
         </div>
     )
